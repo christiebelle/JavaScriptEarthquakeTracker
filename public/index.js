c09df730fb8069ba6f4a69b0bb3a5ab2851532f3
Christiebelle <cslester@outlook.com>
2018-04-15 18:13:32 +0100

    moving functionality around returns the same result

--- public/index.js
+++ public/index.js
@@ -279,7 +279,7 @@ const displayQuakeInfoSig = function (quakesArraySig) {
   const selectedEarthquake = document.querySelector('select')
   selectedEarthquake.addEventListener('change', function() {
     let quake = quakesArraySig[this.value]
-    quakeInfo(quake)
+    quakeInfoSig(quake);
   })
 }
 
