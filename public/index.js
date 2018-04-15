4a81015cd841bd86f07c869bf833ad8b7b39e886
Christiebelle <cslester@outlook.com>
2018-04-15 15:11:35 +0100

    dropdown menu for small quakes created and populated

--- public/index.js
+++ public/index.js
@@ -1,11 +1,14 @@
 const app = function(){
 
     const api = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+    const apiS = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson";
     makeRequestAll(api, requestCompleteAll);
+    makeRequestSmall(apiS, requestCompleteSml);
 };
 
 window.addEventListener('load', app);
 
+//ALL EARTHQUAKES
 const makeRequestAll = function(api, callback){
   const request = new XMLHttpRequest();
   //open request.
@@ -23,6 +26,35 @@ const requestCompleteAll = function(){
     const quakesArray = quakes.features;
     displayQuakeData(quakesArray);
 };
+
+//SMALLL EARTHQUAKES
+const makeRequestSmall = function(api, callback){
+  const request = new XMLHttpRequest();
+  //open request.
+  request.open("GET", api);
+  //what to do when we get a response.
+  request.addEventListener("load", callback)
+  //tell it to run.
+  request.send();
+};
+
+const requestCompleteSml = function(){
+  if(this.status !== 200) return;
+    const jsonString = this.responseText;
+    const quakes = JSON.parse(jsonString);
+    const quakesArraySml = quakes.features;
+    displayQuakeDataSml(quakesArraySml);
+};
+
+const displayQuakeDataSml = function(quakesArraySml){
+  const select = document.getElementById('quakelistSmall-select')
+  quakesArraySml.forEach(function(quake, index) {
+    let option = document.createElement('option')
+    option.innerText = quake.properties.place;
+    option.value = index
+    select.appendChild(option)
+  })
+}
 
 const displayQuakeData = function(quakesArray){
   const button = document.querySelector("#quakelist");
